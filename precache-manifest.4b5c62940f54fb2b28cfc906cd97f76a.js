83e2d8d3d581abb550978c428da135256a8df820
andres9722 <andres_9722@outlook.com>
2018-10-18 10:21:30 -0500

    Updates

---
+++ precache-manifest.4b5c62940f54fb2b28cfc906cd97f76a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "255cd0f7ab465faad54a",
+    "url": "/blog/static/js/runtime~main.255cd0f7.js"
+  },
+  {
+    "revision": "0bb9c035542faa16dad1",
+    "url": "/blog/static/js/main.0bb9c035.chunk.js"
+  },
+  {
+    "revision": "dde06c1bb6445d174b90",
+    "url": "/blog/static/js/1.dde06c1b.chunk.js"
+  },
+  {
+    "revision": "0bb9c035542faa16dad1",
+    "url": "/blog/static/css/main.27cc9ec5.chunk.css"
+  },
+  {
+    "revision": "e15cfa12c52dba35d9669382f3623e4b",
+    "url": "/blog/index.html"
+  }
+];
