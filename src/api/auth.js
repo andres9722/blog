3cf1848fedd36ff79bb3796823ca3252f25f8e39
andres9722 <andres_9722@outlook.com>
2018-10-24 10:03:50 -0500

    Fixes

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const CLIENT_ID = 'a9b9a9cdd993b995f360'
 const CLIENT_SECRET = '4c689dd1d7bc53aa6482bfb09b2edf6d0436cd29'
-const REDIRECT_URI = 'http://localhost:3000/'
+const REDIRECT_URI = 'https://andres9722.github.io/blog/'
 const ENDPOINT = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user,gist&redirect_uri=${REDIRECT_URI}`
 const TOKEN = 'https://github.com/login/oauth/access_token'
 const USER = 'https://api.github.com/user?access_token='
